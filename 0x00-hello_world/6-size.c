7f3754fdbc717b1f523fb5b48f1edd9e38bb45af
Azeem <111095332+Dovemaker@users.noreply.github.com>
2022-09-09 11:51:24 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,16 +13,3 @@ int main(void)
     printf("Size of a float: %i byte(s)\n", sizeof(float));
     return (0);
 }
-
-8. gcc -S -masm=intel $CFILE
-9. #include <stdio.h>
-#include <unistd.h>
-/**
- * main - Entry point
- * Return:1
- */
-int main(void)
-{
-    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-    return (1);
-}
