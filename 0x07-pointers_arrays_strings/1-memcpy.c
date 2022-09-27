556277bf51cae62ea92b46c6288a886e89a045a6
Akande Azeem <Dovemaker3@gmail.com>
2022-09-27 11:20:37 -0700

    tasks,

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,11 +2,11 @@
 
 /**
  * _memcpy - copies memory area
- * @dest: destination of copy
- * @src: memory area to copy
- * @n: number of bytes to copy
+ *  @dest: destination of copy
+ *  @src: memory area to copy
+ *  @n: number of byte to copy
  *
- * Return: pointer to dest
+ *  Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
