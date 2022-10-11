71583adb7486964dd72a2f2aa4599ce90a770f95
Azeem <111095332+Dovemaker@users.noreply.github.com>
2022-10-11 12:16:36 -0700

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - print name of of this source file
+ *	main - print name of of this source file
  *
- * Return: Always 0.
+ *	Return : Always 0.
  */
+
 int main(void)
+	
 {
-printf("%s\n", __FILE__);
-return (0);
+	
+	printf("%s\n", __FILE__);
+	
+	return (0);
+
 }
