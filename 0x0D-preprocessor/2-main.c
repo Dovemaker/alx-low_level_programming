abbc5ef30044c27395fa366feeda96da16b7fc0b
Akande Azeem <Dovemaker3@gmail.com>
2022-10-11 12:23:53 -0700

    tasks o

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- *	main - print name of of this source file
+ * main - print name of of this source file
  *
- *	Return : Always 0.
+ * Return: Always 0.
  */
 
 int main(void)
 	
 {
-	
+
 	printf("%s\n", __FILE__);
-	
+
 	return (0);
 
 }
