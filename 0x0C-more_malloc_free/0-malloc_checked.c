694c700d234fbce2dc8dccddbf51eb8b52b591a8
Akande Azeem <Dovemaker3@gmail.com>
2022-10-23 13:51:49 -0700

    tasks

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,15 +3,10 @@
 #include <stdlib.h>
 
 /**
-
- * malloc_checked - allocates memory using malloc
-
+ *malloc_checked - allocates memory using malloc
  * @b: memory to allocate
-
  *
-
  * Return: pointer to allocated memory or normal process termination if error
-
  */
 
 void *malloc_checked(unsigned int b)
